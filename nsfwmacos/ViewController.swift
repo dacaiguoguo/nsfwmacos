5e37f14b8a29b18cb742317acf9df6e55881dca1
dacaiguoguo <dacaiguoguo@163.com>
2024-04-22 19:10:58 +0800

    Initial Commit

---
+++ nsfwmacos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  nsfwmacos
+//
+//  Created by yanguo sun on 2024/4/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
